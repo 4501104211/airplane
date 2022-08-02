b5797441a90c01bc560bfa0407a8fbe1302d573a
nguyencaothai <lanhoxga@gmail.com>
2022-08-02 17:47:43 +0700

    set up project

---
+++ src/main/java/com/booking/arline/ArlineApplication.java
@@ -0,0 +1,13 @@
+package com.booking.arline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/booking/arline/ArlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.booking.arline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
